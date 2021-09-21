672bb35a1689c225a661ee1682fca81ca5f4dd19
x <noemail@no.io>
2021-09-21 13:07:47 +0200

    Adds static data and app title.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'McDonaldKiosk';
+  title = 'McDonald\'s Kiosk';
+
+  constructor(private titleService: Title){
+    this.titleService.setTitle(this.title);
+  }
 }
