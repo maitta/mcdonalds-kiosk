fff4d81038b442db67cb635f5a694d1420444a21
x <noemail@no.io>
2022-01-24 15:55:38 +0100

    Adds tests for product comp.

--- src/app/product/product.component.spec.ts
+++ src/app/product/product.component.spec.ts
@@ -30,4 +30,25 @@ describe('ProductComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy()
   })
+
+  it('should display the right image', () => {
+    const productElement: HTMLElement = fixture.nativeElement
+    const img = productElement.querySelector('img')!
+    expect(img.src).toBe(img.baseURI + dummyProduct.photo)
+  })
+
+  it('should display product name and price', () => {
+    const productElement: HTMLElement = fixture.nativeElement
+    expect(productElement.textContent).toContain(dummyProduct.name)
+    expect(productElement.textContent).toContain(dummyProduct.price)
+  })
+
+  it('should render the quantity component only for active products', () => {
+    const productElement: HTMLElement = fixture.nativeElement
+    expect(productElement.querySelector('app-quantity')).toBeNull()
+
+    dummyProduct.active = true
+    fixture.detectChanges()
+    expect(productElement.querySelector('app-quantity')).toBeDefined()
+  })
 })
